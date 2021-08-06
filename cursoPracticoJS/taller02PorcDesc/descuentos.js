04cefd4b8506a4669dc6d15242dd1c2bdb83ba39
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-05 23:40:50 -0400

    Se creo la base de HTML y algunas variables en JS

---
+++ cursoPracticoJS/taller02PorcDesc/descuentos.js
@@ -0,0 +1,5 @@
+const precioOriginal = 100;
+const descuento = 15;
+
+const porcentajePrecioConDescuento = 100 - descuento;
+const precioConDescuento = porcentajePrecioConDescuento / 100 * precioOriginal;
