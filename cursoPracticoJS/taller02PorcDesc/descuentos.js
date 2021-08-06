c6cb2cfd4bf5056dbee4960a90fd059ef1b98186
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-06 09:06:33 -0400

    Se termina la clase agregando las funciones y escribiendo directamente en HTML desde JS

--- cursoPracticoJS/taller02PorcDesc/descuentos.js
+++ cursoPracticoJS/taller02PorcDesc/descuentos.js
@@ -1,17 +1,28 @@
 // const precioOriginal = 120;
 // const descuento = 18;
-
-function calcularPrecioConDescuento(precio, descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
-
-    return precioConDescuento;
-}
-
-
 // console.log({
 //   precioOriginal,
 //   descuento,
 //   porcentajePrecioConDescuento,
 //   precioConDescuento
 // });
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function precioAlHacerClick() {
+    const inputPrice = document.getElementById('InputPrice');
+    const pricevalue = inputPrice.value;
+
+    const inputDiscount = document.getElementById('InputDiscount');
+    const discountvalue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(pricevalue, discountvalue);
+
+    const resultP = document.getElementById('ResultPrice');
+    resultP.innerText = 'El precio con descuento es: $' + precioConDescuento;
+}
