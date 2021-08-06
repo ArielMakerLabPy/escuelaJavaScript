a5f0e835ef157d1e3f1bbbb310fc1015932bd168
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-06 08:18:09 -0400

    Se creo la primera funcion

--- cursoPracticoJS/taller02PorcDesc/descuentos.js
+++ cursoPracticoJS/taller02PorcDesc/descuentos.js
@@ -1,5 +1,17 @@
-const precioOriginal = 100;
-const descuento = 15;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const porcentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = porcentajePrecioConDescuento / 100 * precioOriginal;
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+// console.log({
+//   precioOriginal,
+//   descuento,
+//   porcentajePrecioConDescuento,
+//   precioConDescuento
+// });
