39305cd1c9c04db518e9797a4b1b89224f4c435c
Ariel <arielmakerlab@outlook.com>
2021-08-06 21:20:02 -0400

    Se agregó el nuevo taller de Media aritmetica, mediana y moda y ya se terminó de completar el de media aritmética

--- README.md
+++ README.md
@@ -17,3 +17,9 @@
 - Segundo paso: implemetar fórmulas en JS.
 - Tercer paso: crear funciones.
 - Cuarto paso: integrar JS con HTML
+
+### Taller #3: promedio, mediana y moda
+- Primer paso: definir las fórmulas
+- Segundo paso: implemetar fórmulas en JS.
+- Tercer paso: crear funciones.
+- Cuarto paso: integrar JS con HTML
---
+++ cursoPracticoJS/taller03PromMedianModa/promedio.js
@@ -0,0 +1,23 @@
+const lista1 = [100, 200, 300, 500];
+
+let sumaLista1 = 0;
+for (let i = 0; i < lista1.length; i++) {
+  sumaLista1 += lista1[i];
+}
+
+const promedioLista1 = sumaLista1 / lista1.length;
+
+function calcularMediaAritmetica(lista) {
+//   let sumaLista = 0;
+//   for (let i = 0; i < lista.length; i++) {
+//     sumaLista += lista[i];
+//   }
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+  return sumaLista / lista.length;
+}
