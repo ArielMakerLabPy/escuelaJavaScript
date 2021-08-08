fe81302c86d21dd38ed2090bbd797290adb445c8
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-07 21:00:08 -0400

    Se termina la clase de moda

--- cursoPracticoJS/taller03PromMedianModa/moda.js
+++ cursoPracticoJS/taller03PromMedianModa/moda.js
@@ -1,31 +1,33 @@
-function calcularMediaAritmetica(lista) {
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
+const lista1 = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    1
+];
+
+const lista1Count = {};
+
+lista1.map(
+    function (elemento){
+        if (lista1Count[elemento]){
+            lista1Count[elemento] += 1;
+        } else {
+        lista1Count[elemento] = 1;
         }
-    );
-
-  return sumaLista / lista.length;
-}
-
-const lista1 = [100, 200, 500, 4000000];
-
-const mitaLista1 = parseInt(lista1.length / 2);
-
-function esPar(numerito){
-    if (numerito % 2 === 0) {
-        return true;
     }
-    return false;
-}
+);
 
-let mediana;
+const lista1Array = Object.entries(lista1Count).sort(
+    function (a, b){
+       return a[1] - b[1];
+    }
+);
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitaLista1];
-    const elemento2 = lista1[mitaLista1 - 1];
-    const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2]);
-    mediana = promedioElemento1y2;
-} else {
-    mediana = lista1[mitaLista1];
-}
+const moda = lista1Array[lista1Array.length - 1];
