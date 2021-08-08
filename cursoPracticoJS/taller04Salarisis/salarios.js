a44d237a0f8c2eba2b6732fee07cf53fe6e66c9f
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-07 21:45:22 -0400

    Se termina el cálculo de promedio de salario de un array de personas incando sus salarios

---
+++ cursoPracticoJS/taller04Salarisis/salarios.js
@@ -0,0 +1,39 @@
+const colombia = [];
+
+colombia.push({
+    name: 'Juan',
+    salary: 1000
+});
+colombia.push({
+    name: 'Natalia',
+    salary: 1500
+});
+colombia.push({
+    name: 'Javier',
+    salary: 700
+});
+colombia.push({
+    name: 'Herme',
+    salary: 1100
+});
+colombia.push({
+    name: 'Ariel',
+    salary: 2000
+});
+colombia.push({
+    name: 'Ivan',
+    salary: 500
+});
+colombia.push({
+    name: 'Ana',
+    salary: 600
+});
+colombia.push({
+    name: 'Fany',
+    salary: 2200
+});
+colombia.push({
+    name: 'Osvaldo',
+    salary: 1700
+});
+
