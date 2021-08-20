68c76b50eb89b3e6be89a699588ba76b07681635
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-20 08:43:28 -0400

    Se termina el capitulo de parametros por defecto y concatenación con ${} y comillas francesas``

--- cursoECMAScript/es6/index.js
+++ cursoECMAScript/es6/index.js
@@ -1,3 +1,4 @@
+//Versión anterior como agregar datos a variables de nuestra función
 function newFunction(name, age, country) {
   var name = name || 'John Doe';
   var age = age || 30;
@@ -5,10 +6,18 @@ function newFunction(name, age, country) {
   console.log(name, age, country);
 }
 
-//es6
-function newFunction2(name='John Doe', age=30, country='PY') {
-    console.log(name, age, country);
+//es6 para agregar datos a variables de nuestra función y tener variables por defecto
+function newFunction2(name = 'John Doe', age = 30, country = 'PY') {
+  console.log(name, age, country);
 }
 
 newFunction2();
 newFunction2('Ricardo Prada', 40, 'BR');
+//nuevas formas de llamar a variables dentro de texto utilizando ${} y comillas francesas ``
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
