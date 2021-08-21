970dc39d112692b773d6b920dacd905231cf5335
Ariel <arielmakerlab@outlook.com>
2021-08-20 20:57:32 -0400

    Se termina la clase de let, const, multilinea, spread operator y desestructuración

--- cursoECMAScript/es6/index.js
+++ cursoECMAScript/es6/index.js
@@ -21,3 +21,44 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit \n'
++ 'frase epica que necesitamos'
+
+let lorem2 = `Otra frase necesaria para probar un punto aparte
+Y seguimos escribiendo en la parte de abajo`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  'name': 'John Doe',
+  'age': 30,
+  'country': 'PY',
+};
+
+console.log(person.name, person.age, person.country);
+
+let { name, age, } = person;
+console.log(name, age);
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Camila', 'Andres'];
+
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+{
+  var globalVar = 'Global Var';
+
+}
+{
+  let globalLet = 'Global Let';
+}
+
+console.log(globalVar);
+//console.log(globalLet);
+
+const a = 'b';
+//a = 'a';
+console.log(a);
