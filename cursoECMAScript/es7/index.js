49dee7900b936d9841fa281552ec0842d9ba88fe
Ariel Martinez <arielmakerlab@outlook.com>
2021-08-27 08:18:18 -0400

    Se termina .include y exponente ** que se agregaron en EC7

---
+++ cursoECMAScript/es7/index.js
@@ -0,0 +1,14 @@
+let numbers = [1, 2, 3, 7, 8, 9];
+
+if (numbers.includes(5)) {
+    console.log('7 si se encuentra en el array');
+} else {
+    console.log('7 no se encuentra en el array');
+}
+
+let base = 4;
+let exp = 3;
+let result = base ** exp;
+console.log(result);
+
+
