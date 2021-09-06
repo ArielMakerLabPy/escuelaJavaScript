e5fa7d5a3d6769acd71e5c66d22284d6df4f0b98
Ariel Martinez <arielmakerlab@outlook.com>
2021-09-05 21:45:59 -0400

    Actualización

--- cursoAsincronismo/src/callback/index.js
+++ cursoAsincronismo/src/callback/index.js
@@ -6,4 +6,18 @@ function calc(num1, num2, callback){
     return callback(num1, num2);
 }
 
-console.log(calc(2, 2, sum));
+console.log(calc(6, 2, sum));
+
+function date(callback){
+    console.log(new Date);
+    setTimeout(function(){
+        let date = new Date;
+        callback(date);
+    }, 3000);
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+
+date(printDate);
